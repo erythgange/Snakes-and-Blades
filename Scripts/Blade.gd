645ca87f60876e8d86488286aa9e155c7b353c95
erythgange <erythjohn.gange@gmail.com>
2024-07-12 20:16:29 +0800

    instantiate help
    struggling on snake body with instantiate

--- Scripts/Blade.gd
+++ Scripts/Blade.gd
@@ -5,8 +5,13 @@ var left: bool = true
 func _process(delta):
 	if Input.is_action_just_released("Dash"):
 		if left == true:
+			$Sprite2D.flip_h = true
 			$BladeAnimation.play("Swing")
+			
 			left = false
 		else: 
+			$Sprite2D.flip_h = false
 			$BladeAnimation.play("Swing_mirror")
+			
 			left = true
+			
--- Scripts/snake.gd
+++ Scripts/snake.gd
@@ -19,47 +19,57 @@ var is_slashing: bool = false
 var is_parrying: bool = false
 var can_speedup: bool = true
 
-@onready var dash_cooldown = $DashCooldown
+# Scenes to instantiate/duplicate (i.e. the body and the tail)
+var snake_body_node = preload("res://Scenes/snake_body_node.tscn")
+
 
 func _physics_process(delta):
+
+	
+
 
 	# gains speed with max limit
 	if speed < max_speed and can_speedup == true: 
 		speed += health * 0.05 
 
-	# Dash
+
+	# Charge and Dash
 	if Input.is_action_pressed("Dash"):
 		charge += .1
 	if Input.is_action_just_released("Dash"):
-		if charge > 5:
-			print ("dashing!")
+		if charge > 3:
 			$DashTime.start()
 			is_dashing = true
-			bonus_speed = 1000
+			bonus_speed = speed
 		charge = 0
-		
-	
+
+
 	# Rotates snake head with left or right
 	if Input.is_action_pressed("Left"):
 		rotate(deg_to_rad( -(turn + bonus_turn_speed) * delta) )
 	if Input.is_action_pressed("Right"):
 		rotate(deg_to_rad( (turn + bonus_turn_speed) * delta) )
-		
-	
-	
-	# Speed = Movement speed with a max capacity
-	velocity = Vector2(speed+bonus_speed,0).rotated(rotation) 
-	#velocity = velocity.limit_length(max_speed)
-	
+
+
 	# Debug print
-	print(charge)
-	
+	print(speed)
+
+
+	#dont worry about it
+	velocity = Vector2(speed+bonus_speed,0).rotated(rotation) 
 	move_and_slide()
 
+func add_body():
+#for n in range(1,health): #range of one to current health i.e. number of bodies
+	
+	var body = snake_body_node.instantiate()
+	body.position = Vector2(10,0)
+	add_child(body)
 
 
-
-
+func snake_body_instance():
+	health	
+	
 func _on_dash_time_timeout():
 	is_dashing = false
 	bonus_speed = 0
