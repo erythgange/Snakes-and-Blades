e364faa81df37aefa534f395a063625cd1912209
erythgange <erythjohn.gange@gmail.com>
2024-07-12 14:51:16 +0800

    Cleaning up code with labels
    now more readable

--- Scripts/snake.gd
+++ Scripts/snake.gd
@@ -1,33 +1,51 @@
 extends CharacterBody2D
 
-@export var Health = 10
-@export var Speed = 1000
-@export var maxSpeed = Health*100
-@export var Turn = Speed
-#States: 1 = Neutral, 2 = Dashing, 3 = Parrying
-@export var State = 1
+# Base values
+@export var health = 10
+@export var speed = 100
 
-@onready var timer = $Timer
+# Variables
+@export var max_speed = health*100
+@export var turn = (speed/10) + 200
+
+# Bonus variables
+@export var bonus_speed = 0
+@export var bonus_turn_speed = 0
+
+# States
+var is_dashing: bool = false
+var is_slashing: bool = false
+var is_parrying: bool = false
+var can_speedup: bool = true
+
+@onready var timer = $DashCooldown
 
 func _physics_process(delta):
+
+	# gains speed with max limit
+	if speed < max_speed and can_speedup == true: 
+		speed += health * 0.05 
+
+	# Dash
+	if Input.is_action_just_pressed("Dash") and is_dashing == false:
+		print ("charging!")
+	if Input.is_action_just_released("Dash") and is_dashing == false:
+		print ("dashing!")
+		is_dashing = true
 	
-	if Speed < maxSpeed and State == 1:
-		Speed += Health * .05
-	
-	
-	velocity = Vector2(Speed,0).rotated(rotation)
-	velocity = velocity.limit_length(maxSpeed)
-	
-	if State == 1:
-		Turn = (Speed/10) + 100
-	else: Turn = (Speed/10) + 150
-	
+	# Rotates snake head with left or right
 	if Input.is_action_pressed("Left"):
-		rotate(deg_to_rad(-Turn*delta))
+		rotate(deg_to_rad( -(turn + bonus_turn_speed) * delta) )
 	if Input.is_action_pressed("Right"):
-		rotate(deg_to_rad(Turn*delta))
-
+		rotate(deg_to_rad( (turn + bonus_turn_speed) * delta) )
+		
 	
-
-	#print(Speed)
+	
+	# Speed = Movement speed with a max capacity
+	velocity = Vector2(speed+bonus_speed,0).rotated(rotation) 
+	velocity = velocity.limit_length(max_speed)
+	
+	# Debug print
+	print(speed)
+	
 	move_and_slide()
