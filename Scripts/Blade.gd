584a9f4377406885b4cc613eaac3015713c15848
erythgange <erythjohn.gange@gmail.com>
2024-07-12 17:07:17 +0800

    Dash + Attack Animation

--- Scripts/Blade.gd
+++ Scripts/Blade.gd
@@ -1,20 +1,12 @@
 extends Node2D
 
-#States: 1 = Neutral, 2 = Charging(1/3), 3 = Charging(2/3), 4 = Charging(3/3), 5 = Parrying
-var State = 1
+var left: bool = true
 
 func _process(delta):
-	if Input.is_action_just_pressed("Dash") and State == 1:
-		State = 2
-		print("Charging")
-	
-	if Input.is_action_just_released("Dash") and State == 3:
-		print("Dashed!")
-		$DashCooldown.start()
-
-	#print (State)
-
-func _on_cooldown_timer_timeout():
-	State = 1
-	print ("Dash Available")
-	
+	if Input.is_action_just_released("Dash"):
+		if left == true:
+			$BladeAnimation.play("Swing")
+			left = false
+		else: 
+			$BladeAnimation.play("Swing_mirror")
+			left = true
